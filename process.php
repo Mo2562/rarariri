8dd61e88a0bca2c0c44f5bb4b690d6a9333d09df
Mo2562 <55575137+Mo2562@users.noreply.github.com>
2019-12-27 19:22:33 +0900

    Create process.php

---
+++ process.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<HTML lane="ja">
+    <HEAD>
+        <META charset="UTF-8" />
+        <TITLE>処理中</TITLE>
+<meta http-equiv="Refresh" content="3, http://genbu.rmme.do-johodai.ac.jp/~s1612601/zeikatsu/main.php">
+    </HEAD>
+    <BODY>
+	<p>登録しました。</br>
+    3秒後にログインページへ移ります。</p>
+        ページが更新されない場合は、<a href="http://genbu.rmme.do-johodai.ac.jp/~s1612601/zeikatsu/main.php">ここ</a>をクリックしてください
+
+    </BODY>
+</HTML>
